b9394ddf0c334bcda536824dbbd26ec476942f95
nasa-dan.holdaway <nasa-dan.holdaway@noaa.gov>
2018-03-21 18:07:09 -0400

    add nlm pressure calculation

--- src/atmos_cubed_sphere/model_tlmadm/fv_pressures_tlm.F90
+++ src/atmos_cubed_sphere/model_tlmadm/fv_pressures_tlm.F90
@@ -4,9 +4,59 @@ use fv_arrays_mod, only: fv_grid_bounds_type
 
 implicit none
 private
-public compute_pressures_tlm
+public compute_pressures, compute_pressures_tlm
 
 contains
+
+   SUBROUTINE COMPUTE_PRESSURES( bd, npz, kappa, ptop, delp, pe, pk, pkz, peln )
+    IMPLICIT NONE
+!Arguments
+    TYPE(FV_GRID_BOUNDS_TYPE), INTENT(IN) :: bd
+    INTEGER, INTENT(IN) :: npz
+    REAL, INTENT(IN) :: kappa, ptop
+    REAL, INTENT(IN) :: delp(bd%isd:bd%ied, bd%jsd:bd%jed, npz)
+    REAL, INTENT(OUT) :: pe(bd%is-1:bd%ie+1, npz+1, bd%js-1:bd%je+1)
+    REAL, INTENT(OUT) :: pk(bd%is:bd%ie, bd%js:bd%je, npz+1)
+    REAL, INTENT(OUT) :: peln(bd%is:bd%ie, npz+1, bd%js:bd%je)
+    REAL, INTENT(OUT) :: pkz(bd%is:bd%ie, bd%js:bd%je, npz)
+!Locals
+    INTEGER :: i, j, k, is, ie, js, je
+    is = bd%is
+    ie = bd%ie
+    js = bd%js
+    je = bd%je
+    pe(:, :, :) = 0.0
+    pe(:, 1, :) = ptop
+    DO k=2,npz+1
+      DO j=js,je
+        DO i=is,ie
+          pe(i, k, j) = pe(i, k-1, j) + delp(i, j, k-1)
+        END DO
+      END DO
+    END DO
+    DO k=1,npz+1
+      DO j=js,je
+        DO i=is,ie
+          peln(i, k, j) = LOG(pe(i, k, j))
+        END DO
+      END DO
+    END DO
+    DO k=1,npz+1
+      DO j=js,je
+        DO i=is,ie
+          pk(i, j, k) = EXP(kappa*peln(i, k, j))
+        END DO
+      END DO
+    END DO
+    DO k=1,npz
+      DO j=js,je
+        DO i=is,ie
+          pkz(i, j, k) = (pk(i, j, k+1)-pk(i, j, k))/(kappa*(peln(i, k+1&
+&           , j)-peln(i, k, j)))
+        END DO
+      END DO
+    END DO
+  END SUBROUTINE COMPUTE_PRESSURES
 
    SUBROUTINE COMPUTE_PRESSURES_TLM( bd, npz, kappa, ptop, delp, delp_tl, &
                                      pe, pe_tl, pk, pk_tl, pkz, pkz_tl, peln, peln_tl )
