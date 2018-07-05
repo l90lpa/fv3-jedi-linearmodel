00a7b9cb5e695c31de420822f1ed87e79fe97866
danholdaway <danholdaway@mac.com>
2018-07-05 06:38:37 -0400

    add geos physics components

--- src/atmos_cubed_sphere/tools/fv_treat_da_inc.F90
+++ src/atmos_cubed_sphere/tools/fv_treat_da_inc.F90
@@ -32,7 +32,7 @@ module fv_treat_da_inc_mod
   use constants_mod,     only: pi=>pi_8, omega, grav, kappa, &
                                rdgas, rvgas, cp_air
 #else
-  use MAPL_MOD
+  use MAPL_ConstantsMOD
 #endif
   use fv_arrays_mod,     only: fv_atmos_type, &
                                fv_grid_type, &
